f080e9816d5832463155393effbd8321a25992b7
Sean Hanauer <yodaman40@gmail.com>
2019-06-25 16:26:31 -0400

    added the plus and minus buttons, submit now does nothing, reset still works

--- app/src/main/java/com/example/tutorials/MainActivity.java
+++ app/src/main/java/com/example/tutorials/MainActivity.java
@@ -31,25 +31,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * create variables 
+     */
+    int quantity = 0;
+    double price = 4.50 * .07 + 4.5;
+
+    /**
      * This method is called when the order button is clicked.
      */
-    int number = 0;
     public void submitOrder(View view) {
-        display(number + 1);
+        display(quantity);
+        displayPrice(quantity * price);
     }
 
     /**
      * This method adds to the price of the coffee
      */
-    public void displayPrice(int number) {
+    public void displayPrice(double number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    //resets the orders
+    public void reset(View view) {
+        quantity = 0;
+        display(quantity);
+        displayPrice(quantity * price);
+    }
+    //increment quantity by 1
+    public void increment(View view) {
+        quantity ++;
+        display(quantity);
+        displayPrice(quantity * price);
+    }
+
+    public void decrement(View view) {
+        if(quantity > 0) {
+            quantity--;
+            display(quantity);
+            displayPrice(quantity * price);
+        }
+        else{
+            quantity = 0;
+            display(quantity);
+            displayPrice(quantity * price);
+        }
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void display(double number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
